8ac26b93295593a192a767ce18f3213521158dc5
vivek <ikumarvivek786@gmail.com>
2022-10-09 22:24:17 +0530

    Uc-3-CreateQueue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,20 +10,26 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            stack.print();
-            while (!stack.isEmpty())
-            {
-                Console.WriteLine();
-                stack.peek();
-                stack.pop();
-                stack.print();
-                Console.WriteLine();
-            }
-            Console.ReadLine(); 
+            //Stack stack = new Stack();
+            //stack.push(70);
+            //stack.push(30);
+            //stack.push(56);
+            //stack.print();
+            //while (!stack.isEmpty())
+            //{
+            //    Console.WriteLine();
+            //    stack.peek();
+            //    stack.pop();
+            //    stack.print();
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.print();
+            Console.ReadLine();
         }
     }
 }
---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Queue
+    {
+        public LinkedList linkedlist;
+
+        public Queue()
+        {
+            this.linkedlist = new LinkedList();
+        }
+        public void Enqueue(int data)
+        {
+            linkedlist.append(data);
+            Console.WriteLine("{0} pushed to Queue ", data);
+        }
+
+        public void print()
+        {
+            linkedlist.display();
+        }
+
+    }
+}
