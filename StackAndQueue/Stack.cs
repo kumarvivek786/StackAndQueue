5f97cce75db0e684dbae8c4bbe27bc68194f52e5
vivek <ikumarvivek786@gmail.com>
2022-10-09 21:28:43 +0530

    Uc-1CreateStack

---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+
+    public class Stack
+    {
+        public LinkedList linkedlist;
+
+        public Stack()
+        {
+            this.linkedlist = new LinkedList();
+        }
+
+        public void push(int data)
+        {
+            linkedlist.add(data);
+            Console.WriteLine("{0} pushed to stack ", data);
+        }
+        
+
+    }
+}
